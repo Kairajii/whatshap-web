541427b880ab7a155f4f5efb875414fc5f9eeae7
TusharDev456 <tusharkaira@theprofessionalworld.com>
2024-04-08 18:02:35 +0530

    added socket

--- server/index.js
+++ server/index.js
@@ -2,22 +2,68 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 import userRoutes from './src/routes/user.route.js'
-import chatRoutes from './src/routes/chat.route.js';
-import messageRoutes from './src/routes/message.route.js'
+
+import { Server } from "socket.io";
+import { createServer } from "http";
+import User from './src/models/user.model.js';
+import cookieParser from "cookie-parser";
 import cors from 'cors'
 dotenv.config();
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST","DELETE","PUT"],
+    credentials: true,
+  },
+});
 app.use(express.json());
+
 
 app.use(cors())
 const PORT = 5000;
 connectDB();
 app.use('/v1/user',userRoutes)
-app.use('/v1/chat',chatRoutes)
-app.use('/v1/message',messageRoutes)
+
+
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(PORT,()=>console.log(`Server is runnning on the port: ${PORT}`))
+
+const messages = [];
+const users = {};
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    socket.on('join', async (userId) => {
+      const user = await User.findById(userId);
+      if (user) {
+        users[userId] = socket.id;
+        socket.join(userId.toString()); // Join room based on user ID
+      }
+    });
+  
+    socket.on('message', async (data) => {
+      const { from, to, message } = data;
+  
+      // Emit the message to both users in the room
+      io.to(from).to(to).emit('message', { from, message });
+    });
+  
+    socket.on('disconnect', () => {
+      for (let userId in users) {
+        if (users[userId] === socket.id) {
+          delete users[userId];
+        }
+      }
+    });
+  });
+  
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
